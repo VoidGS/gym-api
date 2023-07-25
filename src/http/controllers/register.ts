338f4357c36dd0b97d495dcf16274716e2e1d6aa
VoidGS <guilherme.garcia.s887@gmail.com>
2023-07-25 10:48:34 -0300

    feat: better organization for users logic, and errors handler to users route

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,6 @@
-import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,13 +13,22 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
 	const { name, email, password } = registerBodySchema.parse(request.body)
 
-	await prisma.user.create({
-		data: {
+	try {
+		const prismaUsersRepository = new PrismaUsersRepository()
+		const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+		await registerUseCase.execute({
 			name,
 			email,
-			password_hash: password,
-		},
-	})
+			password,
+		})
+	} catch (err) {
+		if (err instanceof UserAlreadyExistsError) {
+			return reply.status(409).send({ message: err.message })
+		}
+
+		throw err
+	}
 
 	return reply.status(201).send()
 }
