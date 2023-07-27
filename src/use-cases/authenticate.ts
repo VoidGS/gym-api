191059d28490d7da4ae2f3b02ad3f17520ae5b50
VoidGS <guilherme.garcia.s887@gmail.com>
2023-07-26 22:48:29 -0300

    fix: typo on interface of auth

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -3,7 +3,7 @@ import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { compare } from 'bcryptjs'
 import { User } from '@prisma/client'
 
-interface AutheticateUseCaseRequest {
+interface AuthenticateUseCaseRequest {
 	email: string
 	password: string
 }
@@ -18,7 +18,7 @@ export class AuthenticateUseCase {
 	async execute({
 		email,
 		password,
-	}: AutheticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+	}: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
 		const user = await this.usersRepository.findByEmail(email)
 
 		if (!user) {
